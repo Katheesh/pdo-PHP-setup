99a70f52702831008accd283cb113b3db117e154
Katheeskumar Sithamparapillai <katheeskumar@outlook.com>
2020-05-31 09:28:23 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+<title>Welcome to news channel</title>
+    <link rel="stylesheet" type="text/css" href="design/style.css">
+</head>
+<body>
+
+    <div class="container">
+
+        <div class="welcome">
+            <h1>Latest news</h1>
+            <p>Welcome to the demo news site. <em>We never stop until you are aware.</em></p>
+        </div>
+
+        <div class="news-box">
+
+            <div class="news">
+                <h2><a href="read-news.php?newsid=1">First news title here</a></h2>
+                <p> This news short description will be displayed at this particular place. This news short description will be displayed at this particular place.</p>
+                <span>published on Jan, 12th 2015 by zooboole</span>
+            </div>
+
+            <div class="news">
+                <h2><a href="read-news.php?newsid=2">Second news title here</a></h2>
+                <p>This news short description will be displayed at this particular place. This news short description will be displayed at this particular place.</p>
+                <span>published on Jan, 12th 2015 by zooboole</span>
+            </div>
+
+            <div class="news">
+                <h2><a href="read-news.php?newsid=3">Thirst news title here</a></h2>
+                <p>This news short description will be displayed at this particular place. This news short description will be displayed at this particular place.</p>
+                <span>published on Jan, 12th 2015 by zooboole</span>
+            </div>
+
+            <div class="news">
+                <h2><a href="read-news.php?newsid=4">Fourth news title here</a></h2>
+                <p>This news short description will be displayed at this particular place. This news short description will be displayed at this particular place.</p>
+                <span>published on Jan, 12th 2015 by zooboole</span>
+            </div>
+
+        </div>
+
+        <div class="footer">
+            phpocean.com Â© <?= date("Y") ?> - all rights reserved.
+        </div>
+
+    </div>
+</body>
+</html>
